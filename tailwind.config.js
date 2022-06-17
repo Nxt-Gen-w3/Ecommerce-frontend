3feda10a9e06d806ee99d2200f6eb487824edef8
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2022-06-18 01:09:07 +0530

    work on changes in Home & Terms page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      '.hover\:animate-ping:hover': {
+    'animation': 'ping 5s cubic-bezier(0, 0, 0.5, 1) infinite'
+}
+    },
   },
   plugins: [],
 };
